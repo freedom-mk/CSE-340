3494f49a7107c2e2e355bad290baeaf4d4813af2
freedom-mk <fcworldpower@gmail.com>
2025-06-05 22:37:37 +0200

    Add files via upload

---
+++ middleware/validate.js
@@ -0,0 +1,7 @@
+module.exports = function validate(validatorFn) {
+  return (req, res, next) => {
+    const errors = validatorFn(req.body);
+    if (errors.length > 0) return res.status(400).json({ errors });
+    next();
+  };
+};
