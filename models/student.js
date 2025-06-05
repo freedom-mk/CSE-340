d219cafcb5c4d6e8fc254f317022c78d5641e8e2
freedom-mk <fcworldpower@gmail.com>
2025-06-05 22:39:32 +0200

    Add files via upload

---
+++ models/student.js
@@ -0,0 +1,9 @@
+module.exports = {
+  coursesTitle: 'string',
+  courseId: 'string',
+  instructor: 'string',
+  classMax: 'string',
+  currentEnrollment: 'string',
+  startDate: 'string',
+  endDate: 'string'
+};
